f01630ba56110a833cd348701acb7666a3dc188e
Aleksandar Simic <asimic@gmail.com>
2014-02-22 18:03:31 +0000

    omniref mode added

---
+++ omniref.el
@@ -0,0 +1,46 @@
+;; omniref.el --- Omniref Ruby documentation search engine interface
+
+;; Filename: omniref.el
+;; Description: Oniref Ruby documentation search engine interface
+;; Author: "Aleksandar Simić" <asimic@gmail.com>
+;; License: BSD
+;; Created: 2014-02-22 17:22
+;; Version: 1.0.0
+;; URL: http://github.org/dotemacs/omniref.el
+;; Keywords: docs, help, tools
+
+;; This file in not part of GNU Emacs.
+
+;; Comentary:
+
+;; M-x omniref - if a region is selected, then that region is searched
+;; otherwise you'll be prompted for a search term
+
+;;; Code:
+
+(require 'browse-url)
+
+(defconst omniref-url "http://www.omniref.com/?q="
+  "Omniref query URL")
+
+(defun formatted-search-term (terms)
+  "Format the term for searching"
+  (replace-regexp-in-string "[[:space:]]+" "+" terms))
+
+(defun search-for (term)
+  "Browse to Omniref with the search term"
+  (browse-url
+   (concat omniref-url (formatted-search-term term))))
+
+(defun omniref (keywords)
+  "Search Omniref for a word or region"
+  (interactive
+   (list
+    (if (use-region-p)
+	(buffer-substring (region-beginning) (region-end))
+      (read-string "Search omniref for: " (thing-at-point 'word)))))
+  (search-for keywords))
+
+(provide 'omniref)
+
+;;; omniref.el ends here
